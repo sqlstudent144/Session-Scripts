3456f9d3b9c347a49b314109593d1c37bb1bb305
sqlstudent144 <sqlstudent144@gmail.com>
2023-01-21 16:40:23 -0600

    Add files via upload

---
+++ 1 - LoginFails.sql
@@ -0,0 +1,18 @@
+﻿/*I can't log in, I'm getting this error: Login failed for user ''.
+Flying🐒
+Scare🐦- SimplePassword@1
+MySQLId - MySQLId
+*/
+
+
+/* Simple script to check a SQL authenticated login 
+and unlock it if it's locked.*/
+IF LOGINPROPERTY('SQLID','islocked') = 1
+BEGIN
+    ALTER LOGIN SQLID WITH CHECK_POLICY=OFF;
+    ALTER LOGIN SQLID WITH CHECK_POLICY=ON;
+END
+GO
+/* -------------------------------------------------------
+Check the default database of this id.*/
+SELECT * FROM sys.server_principals WHERE name = 'MySQLId'
